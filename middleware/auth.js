3aa2d3d041a2a4d2a9f4f1bf11ca519b9b67337d
m-anil-duman <anil.duman00@gmail.com>
2024-08-11 00:12:53 +0300

    server changes and front end full setup

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-import jwt  from "jsonwebtoken";
-export const verifyToken = async (req, res, next) => {
-    try {
-        let token = req.header("Authorization");
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
